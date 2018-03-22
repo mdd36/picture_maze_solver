ac06c95cfd660fa34b5c3f20b20a2ad388e4ff91
Matt Dickson <matthew.d.dickson@duke.edu>
2018-03-21 22:09:23 -0400

    Solver has method solve

---
+++ src/Solver.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Matthew on 3/21/2018.
+//
+
+//using namespace std;
+
+#include "GraphNode.h"
+
+class Solver{
+public:
+    virtual void solve(GraphNode* head, GraphNode* tail);
+};
