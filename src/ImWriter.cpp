6d42801163d9cb5ca4b16f6c38388ef156c98c6d
Matt Dickson <matthew.dickson.137@gmail.com>
2018-08-20 08:50:25 -0700

    Removal of color gradient for picture output

--- src/ImWriter.cpp
+++ src/ImWriter.cpp
@@ -4,12 +4,6 @@
 
 typedef unsigned char unchar;
 
-unchar const S_R = 0;
-unchar const S_B = 255;
-
-unchar const E_R = 255;
-unchar const E_B = 0;
-
 #include <tuple>
 #include <list>
 #include <fstream>
@@ -18,16 +12,7 @@ unchar const E_B = 0;
 
 class ImWriter{
 public:
-    void write(list<tuple<int, int>>* visitOrder, int numPixelsVisited, vector<vector<string>> rgbFile){
-    }
+    void write(std::vector<GraphNode*>* visitOrder, std::string fileName){
 
-private:
-    string calcRGB(float pos, int length){
-        float n = pos / (float) length;
-        stringstream ss;
-        unchar R = (float) S_R * (1.0f-n) + (float) E_R * n;
-        unchar B = (float) S_B * (1.0f-n) + (float) E_B * n;
-        ss << "(" << R << ", 0, " << B << ")";
-        return ss.str();
     }
 };
